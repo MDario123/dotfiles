ca49fc5ec094d63cc3555c9d73ef14a01c0049ee
Manuel Dario Oliver Ballesteros <manuel.dario.oliver@gmail.com>
2023-10-30 15:55:33 +0000

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,9 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias vim nvim
+    alias please sudo
     set -g fish_greeting
+    set EDITOR vim
 end
 
 export $(/usr/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)
